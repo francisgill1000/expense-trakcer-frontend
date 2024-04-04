15a1a20ae069559962e24647512d2a40fb9c8d20
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-04-05 00:42:11 +0400

    added employee list create delete functionlity

---
+++ components/Employee/Create.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="900">
+      <template v-slot:activator="{ on, attrs }">
+        <v-icon right color="primary" v-bind="attrs" v-on="on"
+          >mdi-plus-circle-outline</v-icon
+        >
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <div class="headline text-centers">Create your employee</div>
+          <v-spacer></v-spacer>
+          <v-icon @click="dialog = false" color="primary">mdi-close</v-icon>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="6">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="payload.name"
+                      label="Name"
+                      required
+                      outlined
+                      dense
+                      hide-details
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="payload.email"
+                      label="Email"
+                      required
+                      outlined
+                      dense
+                      hide-details
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="payload.phone"
+                      label="Phone"
+                      required
+                      outlined
+                      dense
+                      hide-details
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <DatePicker
+                      label="Date of Birth"
+                      @date="
+                        (e) => {
+                          payload.date_of_birth = e.date;
+                        }
+                      "
+                    />
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="payload.address"
+                      label="Address"
+                      required
+                      outlined
+                      dense
+                      hide-details
+                    ></v-text-field>
+                  </v-col>
+
+                </v-row>
+              </v-col>
+              <v-col cols="6">
+                <UploadAttachment @uploaded="handleAttachment" />
+              </v-col>
+              <v-col cols="4">
+                <v-text-field
+                  v-model="payload.position"
+                  label="Position"
+                  required
+                  outlined
+                  dense
+                  hide-details
+                ></v-text-field>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field
+                  v-model="payload.salary"
+                  label="Salary"
+                  required
+                  outlined
+                  dense
+                  hide-details
+                ></v-text-field>
+              </v-col>
+              <v-col cols="4">
+                <DatePicker
+                  label="Hire Date"
+                  @date="
+                    (e) => {
+                      payload.hire_date = e.date;
+                    }
+                  "
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="payload.legal_id"
+                  label="Legal Id"
+                  required
+                  outlined
+                  dense
+                  hide-details
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="payload.local_legal_id"
+                  label="Local Legal Id"
+                  required
+                  outlined
+                  dense
+                  hide-details
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" class="error--text" v-if="errorResponse">
+                {{ errorResponse }}
+              </v-col>
+              <v-col cols="12">
+                <v-btn type="submit" color="primary" @click="submit"
+                  >Submit</v-btn
+                >
+                <v-btn type="submit" @click="dialog = false">Close</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      payload: {},
+      dialog: false,
+      label: "",
+      amount: "",
+      src: "",
+      errorResponse: null,
+      data: [],
+    };
+  },
+  async created() {},
+  methods: {
+    handleAttachment(e) {
+      this.payload.profile_picture = e;
+    },
+    async submit() {
+      try {
+        let formData = new FormData();
+
+        formData.append("name", this.payload.name);
+        formData.append("email", this.payload.email);
+        formData.append("phone", this.payload.phone);
+        formData.append("address", this.payload.address);
+        formData.append("date_of_birth", this.payload.date_of_birth);
+        formData.append("position", this.payload.position);
+        formData.append("salary", this.payload.salary);
+        formData.append("hire_date", this.payload.hire_date);
+        formData.append("legal_id", this.payload.legal_id);
+        formData.append("local_legal_id", this.payload.local_legal_id);
+
+        // Check if profile_picture is present before appending
+        if (this.payload.profile_picture) {
+          formData.append("profile_picture", this.payload.profile_picture);
+        }
+
+        await this.$axios.post("employee", formData);
+
+        // Reset form fields after submitting
+        this.$emit("success", "item has been created");
+        this.dialog = false;
+        // Optionally, you can fetch upsrcd data from the server or perform other actions
+      } catch ({ response }) {
+        this.errorResponse = response.data.message;
+      }
+    },
+  },
+};
+</script>
---
+++ components/Employee/index.vue
@@ -0,0 +1,329 @@
+<template>
+  <v-row no-gutters>
+    <v-navigation-drawer absolute top v-model="filterForm">
+      <v-toolbar dense flat
+        >Filters <v-spacer />
+        <v-icon @click="filterForm = false"
+          >mdi-close-circle-outline</v-icon
+        ></v-toolbar
+      >
+      <v-row no-gutters>
+        <v-col cols="12">
+          <DatePicker
+            label="Start Date"
+            paramKey="start_date"
+            @date="getDateEvent"
+          />
+        </v-col>
+        <v-col cols="12">
+          <DatePicker
+            label="End Date"
+            paramKey="end_date"
+            @date="getDateEvent"
+          />
+        </v-col>
+        <v-col cols="12">
+          <div class="ma-2">
+            <v-btn @click="getDataFromApi">Submit</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-navigation-drawer>
+    <!-- <v-dialog v-model="filterForm">
+      <v-card>
+        <v-card-title>
+          Filters <v-spacer></v-spacer
+          ><v-icon @click="filterForm = false"
+            >mdi-close-circle-outline</v-icon
+          ></v-card-title
+        >
+        <v-card-text>
+          <v-row no-gutters>
+            <v-col cols="12">
+              <DatePicker
+                label="Start Date"
+                paramKey="start_date"
+                @date="getDateEvent"
+              />
+            </v-col>
+            <v-col cols="12">
+              <DatePicker
+                label="End Date"
+                paramKey="end_date"
+                @date="getDateEvent"
+              />
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog> -->
+    <v-col md="12" sm="12">
+      <v-toolbar class="my-1" color="primary" dark rounded dense>
+        <v-toolbar-title>
+          <span>{{ Model }} </span>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-data-table
+        dense
+        :headers="headers"
+        :items="data"
+        :loading="loading"
+        :options.sync="options"
+        :footer-props="{
+          itemsPerPageOptions: [15, 30, 50],
+        }"
+        class="elevation-1"
+        :server-items-length="totalRowsCount"
+      >
+        <template v-slot:top>
+          <v-toolbar class="mb-2" dense flat>
+            <span>
+              <v-icon class="ml-2" @click="reload" color="primary"
+                >mdi-reload</v-icon
+              >
+            </span>
+            <span>
+              <v-icon class="ml-2" @click="filterForm = true" color="primary"
+                >mdi-filter-outline</v-icon
+              >
+            </span>
+
+            <v-spacer></v-spacer>
+            <EmployeeCreate @success="handleSuccess" />
+          </v-toolbar>
+        </template>
+        <template v-slot:item.profile_picture="{ item }">
+          <v-row>
+            <v-col cols="2">
+              <v-avatar size="50" class="my-3">
+                <v-img style="width: 100%" :src="item.profile_picture"></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col>
+              <div class="mt-2">
+                <strong>{{item.name}}</strong>
+              </div>
+              <small>{{item.email}}</small>
+              <br>
+              <small>{{item.phone}}</small>
+            </v-col>
+          </v-row>
+        </template>
+
+        <template v-slot:item.options="{ item }">
+          <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark-2 icon v-bind="attrs" v-on="on">
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-list width="120" dense>
+              <v-list-item @click="viewItem(item)">
+                <v-list-item-title style="cursor: pointer">
+                  <v-icon color="secondary" small> mdi-eye </v-icon>
+                  View
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title style="cursor: pointer">
+                  <EmployeeEdit @success="handleSuccess" :item="item" />
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title style="cursor: pointer">
+                  <Delete
+                    @success="handleSuccess"
+                    :id="item.id"
+                    :endpoint="endpoint"
+                  />
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+      </v-data-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    filterForm: false,
+    Model: "Item",
+    endpoint: "employee",
+    headers: [
+      {
+        text: "Employee",
+        align: "left",
+        sortable: false,
+        key: "profile_picture",
+        value: "profile_picture",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Address",
+        align: "left",
+        sortable: false,
+        key: "address",
+        value: "address",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Date Of Birth",
+        align: "left",
+        sortable: false,
+        key: "date_of_birth",
+        value: "date_of_birth",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Position",
+        align: "left",
+        sortable: false,
+        key: "position",
+        value: "position",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Salary",
+        align: "left",
+        sortable: false,
+        key: "salary",
+        value: "salary",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Hire Date",
+        align: "left",
+        sortable: false,
+        key: "hire_date",
+        value: "hire_date",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Legal Id",
+        align: "left",
+        sortable: false,
+        key: "legal_id",
+        value: "legal_id",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Local Legal Id",
+        align: "left",
+        sortable: false,
+        key: "local_legal_id",
+        value: "local_legal_id",
+        filterable: true,
+        filterSpecial: false,
+      },
+      {
+        text: "Details",
+        align: "left",
+        sortable: false,
+        key: "options",
+        value: "options",
+      },
+    ],
+    data: [],
+    errors: [],
+
+    disabled: false,
+
+    menu: false,
+    payload: {},
+
+    tab: null,
+
+    totalRowsCount: 0,
+    filters: {},
+    isFilter: false,
+    sortBy: "id",
+    sortDesc: false,
+    snack: false,
+    snackColor: "",
+    snackText: "",
+    loading: true,
+    DialogBox: false,
+    response: "",
+    snackbar: false,
+    options: {},
+    search: "",
+    snackbar: false,
+    ids: [],
+    loading: false,
+    //total: 0,
+    editedIndex: -1,
+    response: "",
+    formAction: "Create",
+  }),
+
+  async created() {
+    this.loading = false;
+    this.getDataFromApi();
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    getDateEvent(e) {
+      this.filters = {
+        ...this.filters,
+        ...e,
+      };
+      this.getDataFromApi();
+    },
+    handleSuccess(value) {
+      alert(value || `done`);
+      this.getDataFromApi();
+    },
+    applyFilters() {
+      this.getDataFromApi();
+    },
+    reload() {
+      this.filters = {};
+      this.isFilter = false;
+      this.getDataFromApi();
+    },
+    getDataFromApi() {
+      //this.loading = true;
+      this.loading = true;
+
+      let { sortBy, sortDesc, page, itemsPerPage } = this.options;
+      this.filters.user_id = this.$auth.user.id;
+      let options = {
+        params: {
+          page: page,
+          sortBy: sortBy ? sortBy[0] : "",
+          sortDesc: sortDesc ? sortDesc[0] : "",
+          itemsPerPage: itemsPerPage, //this.pagination.per_page,
+          ...this.filters,
+        },
+      };
+
+      this.$axios.get(this.endpoint, options).then(({ data }) => {
+        this.data = data.data;
+        //this.server_datatable_totalItems = data.total;
+
+        this.totalRowsCount = data.total;
+
+        this.loading = false;
+      });
+    },
+  },
+};
+</script>
